f5ea1746f4c7859c4f79a6815c5799e0d4177178
cindynavrro <cindynavarro@Cindys-MBP-1463.lan>
2021-05-13 15:02:40 -0400

    attempting to resolve failed page build

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-
 dotenv.config();
+
+const dbService = require('./dbService')
 
 app.use(cors());
 app.use(express.json());
