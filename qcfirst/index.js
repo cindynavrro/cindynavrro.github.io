aa03aae889002d629bf5333bf94e5be4d364605e
cindynavrro <cindynavarro@Cindys-MBP-1463.lan>
2021-05-23 13:16:47 -0400

    Successful backend insert/delete courseDB

--- qcfirst/index.js
+++ qcfirst/index.js
@@ -14,7 +14,7 @@ window.onload = function() {
             courseDepartment : document.querySelector('#department').value,
             courseName: document.querySelector('#course-Name').value,
             courseDescription : document.querySelector('#courseDescription').value,
-            courseBeginTime : document.querySelector('#beginTime').value,
+            courseBeginTime : document.querySelector('#beginTime').value.toString(),
             courseEndTime : document.querySelector('#endTime').value,
             courseDays: getCheckedValues(),
             courseCapacity : document.querySelector('#courseCap').value
@@ -48,21 +48,40 @@ function insertRowIntoTable(data){
 
 }
 
+document.querySelector("#table-body").addEventListener('click', function (event){
+    //console.log(event.target.dataset.id);
+    if(event.target.className === "delete-row-btn") {
+        deleteRowById(event.target.dataset.id);
+    }
+})
+
+    function deleteRowById(id) {
+        fetch('http://localhost:5000/delete/' + id, {
+            method: 'DELETE'
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
+
+
 function loadHTMLTable(data) {
-    const table = document.querySelector('#table-body')
+    const table = document.querySelector('#table-body');
     if(data.length === 0){
         table.innerHTML = "<tr><td class='no-data' colspan='3'>No Data</td></tr>";
         return;
     }
-
     let tableHTML = "";
 
-    data.forEach(function ({courseName}) {
+    data.forEach(function ({courseID, courseName, days, beginTime, endTime}) {
         tableHTML += "<tr>";
         tableHTML += `<td>${courseName}</td>`;
+        tableHTML += `<td>${days}</td>`;
+        tableHTML += `<td>${beginTime}-${endTime}</td>`
+        if(document.getElementById("cntr")){
+            tableHTML += `<td><button onclick="location.href='instructorHome.html';" class="delete-row-btn" data-id="${courseID}">Delete</button></td>`
+        }
         tableHTML += "</tr>";
     });
-
     table.innerHTML = tableHTML;
 
 }
@@ -78,3 +97,5 @@ function getCheckedValues() {
     days = days.slice(0, days.length - 1);
     return days;
 }
+
+
